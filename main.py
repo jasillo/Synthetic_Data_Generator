6d913ca1fc7f9bb0c317fa79d7d45eb7874d10b5
jorge asillo <jorge.asillo@gmail.com>
2024-05-31 17:40:12 -0500

    synthetic data
    using jw300, generate syntetic data from english corpus

--- main.py
+++ main.py
@@ -1,27 +1,51 @@
 from noun_tokenizer import *
 from langchain_community.llms import Ollama
 
+# get english part of jw300 corpus
+# id | quechua type (que, quz, quy) | english str | quechua str
+def get_english_corpus(string :str, index : int):
+    splited = string.split(sep="|")
+    return splited[index]
 
 
-# with open('archivo.txt', 'r') as archivo:
-#     # Lee todas las líneas del archivo
-#     lineas = archivo.readlines()
-    
-#     # Itera sobre cada línea
-#     for linea in lineas:
-#         # Procesa cada línea como sea necesario
-#         print(linea.strip()) 
-
-
-sentence =  "the god above us"
-nouns = NounTokenizer(sentence)
 
 llm = Ollama(model="phi3:mini")
-prompt = str.format('''replace the word "{0}" with a similar semantich word in the sentence : "{1}"''',
+counter = 5
+with open('dataset/qu.paralelo.clean', 'r', encoding="utf8") as file:
+    lines = file.readlines()
+    for line in lines:
+        if (counter == 0): break
+        counter -= 1
+
+        sentence =  get_english_corpus(line, 2)
+        nouns = NounTokenizer(sentence)
+        
+        if (len(nouns) == 0):
+            print("NO NOUN")
+            continue
+
+        prompt = str.format('''replace the word "{0}" with a similar semantich word in the sentence : "{1}"''',
                     nouns.firts_noun(),
                     sentence
                     )
+        result = llm.invoke(prompt) 
+        firts_line = substr_get_firts_line(result)
+        if (firts_line == None) :
+            print("NO RESPONSE")
+            continue
 
-result = llm.invoke(prompt)
-print(result)
-print(substr_doble_quote(result))
+        res = substr_doble_quote(firts_line)
+        print("noun : " + nouns.firts_noun())
+        print("original : " + sentence)
+        if (res == None):
+            print("synthetic : " + firts_line)
+        else:
+            print("synthetic : " + res)
+
+        
+        
+
+
+
+# print(result)
+# print(substr_doble_quote(result))
